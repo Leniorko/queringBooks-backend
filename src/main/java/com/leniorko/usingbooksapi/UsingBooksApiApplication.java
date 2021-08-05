74077f889e3eef7f96d6b444eb91dc8a39690084
Ivan Egorov <www.Len244@gmail.com>
2021-08-05 21:29:38 +0300

    Added swagger generated documentation

--- src/main/java/com/leniorko/usingbooksapi/UsingBooksApiApplication.java
+++ src/main/java/com/leniorko/usingbooksapi/UsingBooksApiApplication.java
@@ -3,7 +3,7 @@ package com.leniorko.usingbooksapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class UsingBooksApiApplication {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	String home() {
 		return "Server is up and running.";
 	}
