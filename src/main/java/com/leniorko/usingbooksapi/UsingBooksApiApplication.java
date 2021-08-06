64673d6bb9e8f306c1f7e198b008e6e04a167fd6
Ivan Egorov <www.Len244@gmail.com>
2021-08-06 19:24:38 +0300

    IDK what to change to make it work in Heroku. It is crushes

--- src/main/java/com/leniorko/usingbooksapi/UsingBooksApiApplication.java
+++ src/main/java/com/leniorko/usingbooksapi/UsingBooksApiApplication.java
@@ -1,14 +1,13 @@
 package com.leniorko.usingbooksapi;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@EnableAutoConfiguration
 public class UsingBooksApiApplication {
 
 	@GetMapping("/")
